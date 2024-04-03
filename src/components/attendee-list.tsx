04f81bce670ec8d98311534285c379c46c05d460
robertoDCJ <robertox70@hotmail.com>
2024-04-03 19:43:00 -0300

    ajust config component icon-button

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -8,21 +8,45 @@ import { TableHeader } from './table/table-header'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
 import { attendees } from '../data/attendees'
+import { ChangeEvent, useState } from 'react'
 
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
 
 export function AttendeeList() {
+    const [search, setSearch] = useState('')
+    const [page, setPage] = useState(1)
+
+    const totalPages = Math.ceil(attendees.length / 10)
+
+    function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+        setSearch(event.target.value)
+    }
+
+    function goToFirstPage() {
+        setPage(1)
+    }
+    function goToLastPage() {
+        setPage(totalPages)
+    }
+    function goToNextPage() {
+        setPage(page + 1)
+    }
+
+    function goToPreviousPage() {
+        setPage(page - 1)
+    }
+
     return (
         <div className='flex flex-col gap-4'>
             <div className="flex gap-3 items-center">
                 <h1 className="text-2xl font-bold">Participantes</h1>
                 <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
                     <Search className='size-4 text-emerald-300' />
-                    <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..." />
+                    <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..." />
                 </div>
+                {search}
             </div>
-
             <Table>
                 <thead>
                     <tr className='border-b border-white/10'>
@@ -37,7 +61,7 @@ export function AttendeeList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {attendees.map((attendee) => {
+                    {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
                         return (
                             <TableRow key={attendee.id} >
                                 <TableCell>
@@ -64,22 +88,22 @@ export function AttendeeList() {
                 <tfoot>
                     <tr>
                         <TableCell colSpan={3}>
-                            Mostrando 10 de 228 itens
+                            Mostrando 10 de {attendees.length} itens
                         </TableCell>
                         <TableCell className='text-right' colSpan={3}>
                             <div className='inline-flex items-center gap-8'>
-                                <span>Página 1 de 223</span>
+                                <span>Página {page} de {totalPages}</span>
                                 <div className='flex gap-1.5'>
-                                    <IconButton>
+                                    <IconButton onClick={goToFirstPage} disabled={page === 1}>
                                         <ChevronsLeft className='size-4' />
                                     </IconButton>
-                                    <IconButton>
+                                    <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                                         <ChevronLeft className='size-4' />
                                     </IconButton>
-                                    <IconButton>
+                                    <IconButton onClick={goToNextPage} disabled={page === totalPages}>
                                         <ChevronRight className='size-4' />
                                     </IconButton>
-                                    <IconButton>
+                                    <IconButton onClick={goToLastPage} disabled={page === totalPages}>
                                         <ChevronsRight className='size-4' />
                                     </IconButton>
                                 </div>
